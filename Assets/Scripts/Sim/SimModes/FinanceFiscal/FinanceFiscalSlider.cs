d32d29a4fdd929a5a42c4ffb2bb3a37b95c9d4b9
levimhuillet <levimhuillet@gmail.com>
2022-07-12 17:04:55 -0500

    added fiscal sliders; widened tutorial zone

---
+++ Assets/Scripts/Sim/SimModes/FinanceFiscal/FinanceFiscalSlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Zavala
+{
+    public class FinanceFiscalSlider : MonoBehaviour
+    {
+        [SerializeField] private Slider m_slider;
+        [SerializeField] private TMP_Text m_title;
+
+
+        public Slider Slider {
+            get { return m_slider; }
+        }
+        public TMP_Text Title {
+            get { return m_title; }
+        }
+    }
+}
---
+++ Assets/Scripts/Sim/SimModes/FinanceFiscal/FinanceFiscalUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zavala
+{
+    public class FinanceFiscalUI : SimModeUI
+    {
+        //[Header("FinanceFiscalUI")]
+
+        public override void Open() {
+            InitIndicatorVals();
+        }
+
+        private void InitIndicatorVals() {
+
+        }
+    }
+}
