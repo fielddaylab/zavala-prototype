401100fe55ba73d6ddb864ee91364fc7b9d2abd0
levimhuillet <levimhuillet@gmail.com>
2022-09-23 16:08:20 -0500

    setup of core scripts

---
+++ Assets/Scripts/Functionalities/Produces.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zavala.Resources
+{
+    public enum Type
+    {
+        None,
+        Fertilizer,
+        Grain,
+        Milk,
+        Manure
+    }
+}
+
+namespace Zavala.Functionalities
+{
+    public class Produces : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Grid/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zavala.Tiles
+{
+    public enum Type {
+        None,
+        Land,
+        GrainFarm,
+        DairyFarm,
+        City,
+        Lake
+    }
+
+    public class Tile : MonoBehaviour
+    {
+
+    }
+}
