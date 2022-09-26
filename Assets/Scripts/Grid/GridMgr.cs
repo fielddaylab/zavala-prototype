5e9d12d71bd7d0037f409214e13ea24e3aaf1209
levimhuillet <levimhuillet@gmail.com>
2022-09-26 12:27:01 -0500

    updated unity version; imported placeholder assets; set up initial scene

--- Assets/Scripts/Grid/GridMgr.cs
+++ Assets/Scripts/Grid/GridMgr.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zavala.Tiles;
 
 namespace Zavala
 {
     public class GridMgr : MonoBehaviour
     {
 
+
+        public static List<Tile> GetAdjTiles(Tile centerTile) {
+            List<Tile> adjTiles = new List<Tile>();
+
+            // raycast in 6 directions
+
+            return adjTiles;
+        }
     }
 }
