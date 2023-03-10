3e31cb5bc4088c3d15bc262ffce2815a87a99858
levimhuillet <levimhuillet@gmail.com>
2023-03-09 20:51:40 -0600

    removed errant using statement

--- Assets/Scripts/Functionalities/Requests.cs
+++ Assets/Scripts/Functionalities/Requests.cs
@@ -1,12 +1,7 @@
 ﻿using BeauRoutine;
-using Mono.Cecil;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using Zavala.Events;
 using Zavala.Resources;
